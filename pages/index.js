b995573472ec5a0485fd9794fcbaf545e2bb2928
osydoo <osydooo@gmail.com>
2022-06-01 18:07:59 +0900

    ssn적용중

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/Layout";
-import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>FE Developer</p>
-        <p>
-          <a href="https://github.com/osydoo">https://github.com/osydoo</a>.
-        </p>
-      </section>
-    </Layout>
-  );
+export default async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
