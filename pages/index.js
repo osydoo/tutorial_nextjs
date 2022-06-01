7d25561fd6eb2dabe95c62af48dc67426aeeff4a
윤동우 <osydoo>
2022-06-02 00:35:28 +0900

    dynamic routes 완료

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import { getSortedPostsData } from "../lib/posts";
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/Layout";
 import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
@@ -20,11 +22,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
@@ -40,11 +44,5 @@ export async function getStaticProps() {
     props: {
       allPostsData,
     },
-  };
-}
-
-export async function getServerSideProps(context) {
-  return {
-    props: {},
   };
 }
