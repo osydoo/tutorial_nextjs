4a587159d30d574d1b4768c82fd37e938fe96db1
osydoo <osydooo@gmail.com>
2022-06-01 17:07:51 +0900

    - Assets, Metadata and CSS 완료

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,23 +1,27 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import profilePic from '../../public/images/profile.jpg'
-import Head from 'next/head';
+import Link from "next/link";
+import Image from "next/image";
+import profilePic from "../../public/images/profile.jpg";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
-export default function FirstPost(){
-    return <>
-        <Head>
-            <title>First Post</title>
-        </Head>
-        <h1>First Post</h1>
-        <h2>
-            <Link href={"/"}>
-                <a>Back to Home</a>
-            </Link>
-        </h2>
-        <Image src={profilePic}
-               alt={"Picture of the author"}
-                height={500}
-               width={500}
-        />
-    </>
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href={"/"}>
+          <a>Back to Home</a>
+        </Link>
+      </h2>
+      <Image
+        src={profilePic}
+        alt={"Picture of the author"}
+        height={500}
+        width={500}
+      />
+    </Layout>
+  );
 }
