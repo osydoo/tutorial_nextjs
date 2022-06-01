dee002b7449413dc2d1d2d209315734dcfe7afd5
윤동우 <osydoo>
2022-06-01 23:49:09 +0900

    ssn 수정 완료

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,50 @@
 import { getSortedPostsData } from "../lib/posts";
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/Layout";
+import utilStyles from "../styles/utils.module.css";
 
-export default async function getStaticProps() {
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>FE Developer</p>
+        <p>
+          <a href="https://github.com/osydoo">https://github.com/osydoo</a>.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
 
   return {
     props: {
       allPostsData,
     },
+  };
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {},
   };
 }
